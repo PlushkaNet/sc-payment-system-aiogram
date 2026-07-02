5fa5e319aca388cd8c94527ebe128b88931d2cd8
PlushkaNet <187215446+PlushkaNet@users.noreply.github.com>
2026-07-02 22:19:10 +0300

    add table autocreate in bot.main

--- bot/main.py
+++ bot/main.py
@@ -2,10 +2,18 @@ import os
 import asyncio
 
 from aiogram import Bot, Dispatcher
+import aiosqlite
 import dotenv
+
+from . import variables
 from .commands import nonlogin, security, general, trades
 
 async def main():
+    async with aiosqlite.connect(variables.DATABASE_NAME) as db:
+        await db.execute("CREATE TABLE IF NOT EXISTS USERS(NAME TEXT UNIQUE, PASSWORD TEXT, BALANCE FLOAT)")
+        await db.execute("CREATE TABLE IF NOT EXISTS TRADE_IDS(ID TEXT UNIQUE, COST FLOAT, OWNER_NAME TEXT)")
+        await db.execute("CREATE TABLE IF NOT EXISTS SUBSCRIPTIONS(NAME TEXT UNIQUE, TRADEPACK BOOLEAN, NOADS BOOLEAN)")
+
     dotenv.load_dotenv()
     bot = Bot(os.environ["TOKEN"])
     dp = Dispatcher()
